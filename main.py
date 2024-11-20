d50a1700ee4d218d0d0218479a5ca1363432ad2b
Rizal Anggoro <gnoogler4@gmail.com>
2024-11-20 20:27:41 +0700

    feat: root page

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, responses
 from ml.product_classification import predict_classification, standardize_product_name
 from models.classification import PredictRequest
+
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    """
+    Root endpoint that returns a simple HTML page with a welcome message.
+    """
+    return responses.HTMLResponse(
+        """
+            <!DOCTYPE html>
+            <html>
+                <head>
+                    <title>ML API</title>
+                </head>
+                <body>
+                    <h1>Welcome to the ML API</h1>
+                    <p>Visit the <a href="/docs">API documentation</a> for more information.</p>
+                </body>
+            </html>
+        """
+    )
 
 
 @app.post("/predict")
